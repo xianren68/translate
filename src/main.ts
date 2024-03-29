e130b43894f86ddeac3aad281a183e565514b73b
xianren <xianren711@qq.com>
2024-03-29 18:05:25 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { invoke } from '@tauri-apps/api/tauri'
+import { listen } from "@tauri-apps/api/event";
 import { xrSelect, XrAreaText } from './components'
+import type { Res } from './type'
 window.customElements.define('xr-select', xrSelect)
 window.customElements.define('xr-areatext', XrAreaText)
 
 function main() {
   const inputValue = document.getElementById('inputValue') as HTMLInputElement
   const outputValue = document.getElementById('outputValue') as HTMLInputElement
-  // get input value
-  inputValue.addEventListener('xrInput', (event: any) => {
+  // get translated data.
+  listen<Res>('translate', (event: any) => {
     outputValue.setAttribute('value', event.detail.message)
+  })
+  // get input value.
+  inputValue.addEventListener('xrInput', (event: any) => {
+    console.log(event.detail.message)
   })
 }
 main()
---
+++ src/type/index.ts
@@ -0,0 +1,11 @@
+// form data type for request translate api.
+export type Req = {
+    from : string
+    to : string
+    q : string
+}
+
+// response data type for request translate api.
+export type Res = {
+    
+}
